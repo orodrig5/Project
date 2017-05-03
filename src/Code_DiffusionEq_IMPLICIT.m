bc410daa70ab3387109fc060e2fc3611b4cc8a05
orodrig5 <okeroscar@gmail.com>
2017-05-03 14:40:59 -0500

    Paramenter for Implicit and Explicit have been set

---
+++ src/Code_DiffusionEq_IMPLICIT.m
@@ -0,0 +1,14 @@
+%MECE 5397 PROJECT
+%Oscar Rodriguez
+%10717156   
+%
+%Project B- Diffusion Equation
+%Bc2-4
+
+%Solving Equation Using Explicit Method
+
+%Defining Parameters
+
+ax=-pi;
+ay=-pi;
+bx=pi;
