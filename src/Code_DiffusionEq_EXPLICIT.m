6cd5c1f063a048ff57cac3a9889df1993a31207e
orodrig5 <okeroscar@gmail.com>
2017-05-04 13:01:13 -0500

    Optomized by removing boundaries out of loop

--- src/Code_DiffusionEq_EXPLICIT.m
+++ src/Code_DiffusionEq_EXPLICIT.m
@@ -43,28 +43,29 @@ fa=y.*(y-ay).^2 ;                   %defining f(a)
 ga=((y-ay).^2).*cos(3.14.*y/(ay)); %defining g(a)
 Neuman_ay=0;               %defning the neumand condition given by the proble,             
 u=zeros(Nxx,Nyy);          %Creating the grid for temperature        
-un=zeros(Nxx,Nyy);         %initioal grid for temperature, Initial time condition is also represented by this line       
+un=zeros(Nxx,Nyy);         %initioal grid for temperature, Initial time condition is also represented by this line
+
+u(1,:)=ga; %detriclet non linear  condition in the  right side boundary
+u(Nxx,:)=fa; %detriclet condition non linear condition left side boudary 
+u(:,Nyy)=fa(10)+((x+3.14)/(6.28)*(ga(10)-fa(10))); %detrichlet condition upper boundary 
 for it=0:nt                %Begin time loop
     un=u;                  %definite initial temperature (initial time condition)
     hei=surf(x,y,u');      %Initiation of 3 dimensional graph capable of showing time dependece of problem
     shading interp% color gradient for graph
-       %range of g3D graph
-    title({['2-D Diffusion with D = ',num2str(D)];['time (\itt) = ',num2str(it*delta_t)]})
     xlabel('(x)') %lable axixs
     ylabel('(y)') %lable axis
     zlabel('Temperature') %lable axis
+   
+    title({['2-D Diffusion with D = ',num2str(D)];['time (\itt) = ',num2str(it*delta_t)]})
+    
+    
     drawnow;
     refreshdata(hei) %frefresh of graph per time interval
        %Explicit method:
    for j=2:Nyy-1 %initiation of iterative process to calculate temperature (y axis)
     for i=2:Nxx-1%initiation of iterative process to calculate temperature (y axis)
     u(i,j)=un(i,j)+(D*delta_t*(un(i+1,j)-2*un(i,j)+un(i-1,j))/(h1*h1))+(D*delta_t*(un(i,j+1)-2*un(i,j)+un(i,j-1))/(h1*h1));
-%Boundary conditions
-    
-    u(1,:)=ga; %detriclet non linear  condition in the  right side boundary
-    u(Nxx,:)=fa; %detriclet condition non linear condition left side boudary 
-    u(:,Nyy)=fa(10)+((x+3.14)/(6.28)*(ga(10)-fa(10))); %detrichlet condition upper boundary 
-    %Neumann
+        
     u(:,1)=u(:,2)-Neuman_ay*h1; %Neuman condition bottom boundary
     
     end %end for loop for iteration process of x
