1ae725cf249c741a028bf43711b0c8c709de3321
orodrig5 <okeroscar@gmail.com>
2017-05-03 21:58:29 -0500

    Clean up

--- src/Code_DiffusionEq_EXPLICIT.m
+++ src/Code_DiffusionEq_EXPLICIT.m
@@ -26,10 +26,10 @@ nt=100;                           %Number of time steps
 delta_t=.1;                      %size time step
 h1=6.28/(Nxx-1);                 %Size of space step(x)
 
-x=ax:h1:bx;                        %Range of x(0,2) and specifying the grid points
-y=ay:h1:by;                        %Range of y(0,2) and specifying the grid points
+x=ax:h1:bx;                       
+y=ay:h1:by;                        
 
-lamnda=((2*D*delta_t)/(h1^2));
+lamnda=((2*D*delta_t)/(h1^2));  %lamnda to measure stability of the operation
 
 if lamnda<=.5
     
@@ -41,17 +41,12 @@ end
                     
 fa=y.*(y-ay).^2;
 ga=((y-ay).^2).*cos(3.14.*y/(ay));
-
-UnW=0;                           %x=0 Neumann B.C (du/dn=UnW)
-UnE=0;                           %x=L Neumann B.C (du/dn=UnE)
-UnS=0;                           %y=0 Neumann B.C (du/dn=UnS)
-UnN=0;                           %y=L Neumann B.C (du/n=UnN)
-
-u=zeros(Nxx,Nyy);                  %Preallocating u
-un=zeros(Nxx,Nyy);                 %Preallocating un
+Neum_ay=0;                          
+u=zeros(Nxx,Nyy);                 
+un=zeros(Nxx,Nyy);                 
 for it=0:nt
     un=u;
-    h=surf(x,y,u);       %plotting the field variable
+    h=surf(x,y,u);      
     shading interp
     axis ([-3.14 3.14 -3.14 3.14 -3.14 3.14])
     title({['2-D Diffusion with {\nu} = ',num2str(D)];['time (\itt) = ',num2str(it*delta_t)]})
@@ -64,14 +59,13 @@ for it=0:nt
    for j=2:Nyy-1 
     for i=2:Nxx-1
     u(i,j)=un(i,j)+(D*delta_t*(un(i+1,j)-2*un(i,j)+un(i-1,j))/(h1*h1))+(D*delta_t*(un(i,j+1)-2*un(i,j)+un(i,j-1))/(h1*h1));
-    %Boundary conditions
-    %Dirichlet:
+%Boundary conditions
+    %Dirichlet
     u(1,:)=ga;
     u(Nxx,:)=fa;
     u(:,Nyy)=fa(10)+((x+3.14)/(6.28)*(ga(10)-fa(10)));
-    %Neumann:
-    
-   u(:,1)=u(:,2)-UnS*h1;
+    %Neumann
+    u(:,1)=u(:,2)-Neum_ay*h1;
     
    end
    end
