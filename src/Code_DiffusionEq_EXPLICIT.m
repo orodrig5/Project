41c89aeceb1467effba661298936b5c025593cea
orodrig5 <okeroscar@gmail.com>
2017-05-03 21:27:18 -0500

    Fucntional graph

--- src/Code_DiffusionEq_EXPLICIT.m
+++ src/Code_DiffusionEq_EXPLICIT.m
@@ -22,7 +22,7 @@ Nxx=10;                           %Number of steps in space(x)
 Nyy=10;                           %Number of steps in space(y)       
 
 
-nt=300;                           %Number of time steps 
+nt=100;                           %Number of time steps 
 delta_t=.1;                      %size time step
 h1=6.28/(Nxx-1);                 %Size of space step(x)
 
@@ -39,11 +39,9 @@ else
 end
 
                     
+fa=y.*(y-ay).^2;
+ga=((y-ay).^2).*cos(3.14.*y/(ay));
 
-UW=1;                            %x=0 Dirichlet B.C 
-UE=0;                            %x=L Dirichlet B.C 
-US=0;                            %y=0 Dirichlet B.C 
-UN=0;                            %y=L Dirichlet B.C 
 UnW=0;                           %x=0 Neumann B.C (du/dn=UnW)
 UnE=0;                           %x=L Neumann B.C (du/dn=UnE)
 UnS=0;                           %y=0 Neumann B.C (du/dn=UnS)
@@ -53,32 +51,28 @@ u=zeros(Nxx,Nyy);                  %Preallocating u
 un=zeros(Nxx,Nyy);                 %Preallocating un
 for it=0:nt
     un=u;
-    h=surf(x,y,u','EdgeColor','none');       %plotting the field variable
+    h=surf(x,y,u);       %plotting the field variable
     shading interp
     axis ([-3.14 3.14 -3.14 3.14 -3.14 3.14])
     title({['2-D Diffusion with {\nu} = ',num2str(D)];['time (\itt) = ',num2str(it*delta_t)]})
-    xlabel('Spatial co-ordinate (x) \rightarrow')
-    ylabel('{\leftarrow} Spatial co-ordinate (y)')
-    zlabel('Transport property profile (u) \rightarrow')
+    xlabel('(x)')
+    ylabel('(y)')
+    zlabel('Temperature')
     drawnow; 
     refreshdata(h)
        %Explicit method:
    for j=2:Nyy-1 
-   for i=2:Nxx-1
-    
-
+    for i=2:Nxx-1
     u(i,j)=un(i,j)+(D*delta_t*(un(i+1,j)-2*un(i,j)+un(i-1,j))/(h1*h1))+(D*delta_t*(un(i,j+1)-2*un(i,j)+un(i,j-1))/(h1*h1));
     %Boundary conditions
     %Dirichlet:
-    u(1,:)=UW;
-%     u(nx,:)=UE;
-   u(:,1)=US;
-%     u(:,ny)=UN;
+    u(1,:)=ga;
+    u(Nxx,:)=fa;
+    u(:,Nyy)=fa(10)+((x+3.14)/(6.28)*(ga(10)-fa(10)));
     %Neumann:
-    %u(1,:)=u(2,:)-UnW*h1;
-    u(Nxx,:)=u(Nxx-1,:)+UnE*h1;
-   % u(:,1)=u(:,2)-UnS*dy;
-    u(:,Nyy)=u(:,Nyy-1)+UnN*h1;
+    
+   u(:,1)=u(:,2)-UnS*h1;
+    
    end
    end
    
